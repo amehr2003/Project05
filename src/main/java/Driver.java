02eaf9942e091ef63890d9df4b9adea490f10822
jafurman <jafbird52@gmail.com>
2022-05-09 23:02:43 -0700

    Made changes to driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,16 +1,26 @@
-package main.java;
+import main.java.GraphImplementation;
 
 public class Driver
 {
     public static void main(String args [])
     {
         GraphImplementation driverGraph = new GraphImplementation(7);
-        driverGraph.addEdge("A", "B");
-        driverGraph.addEdge("A", "B");
-        driverGraph.addEdge("A", "B");
-        driverGraph.addEdge("A", "B");
-        driverGraph.addEdge("A", "B");
-        driverGraph.addEdge("A", "B");
-        driverGraph.addEdge("A", "B");
+
+//with seven nodes we have 14 edges
+        driverGraph.addEdge(0,1);
+        driverGraph.addEdge(1,0);
+        driverGraph.addEdge(2,1);
+        driverGraph.addEdge(1,2);
+        driverGraph.addEdge(3,2);
+        driverGraph.addEdge(2,3);
+        driverGraph.addEdge(4,3);
+        driverGraph.addEdge(3,4);
+        driverGraph.addEdge(5,4);
+        driverGraph.addEdge(4,5);
+        driverGraph.addEdge(6,5);
+        driverGraph.addEdge(5,6);
+        driverGraph.addEdge(7,6);
+        driverGraph.addEdge(6,7);
+        
     }
 }
---
+++ src/main/java/main/java/GraphImplementation.java
@@ -0,0 +1,99 @@
+package main.java;
+import java.util.*;
+
+
+public class GraphImplementation {
+/**
+ this is old code
+
+ private LinkedList<Integer> adj_list[];
+
+    Queue<Object> traversalOrder = new LinkedList<> ();
+    Queue<Object> vertexQueue = new LinkedList<>();
+
+    /*
+    GraphImplementation(int v)
+    {
+        int V = v;
+        adj_list = new LinkedList[v];
+        for (int i=0; i<v; ++i)
+            adj_list[i] = new LinkedList();
+    }
+ **/
+
+    //new code
+    Queue<Object> traversalOrder = new LinkedList<> ();
+    Queue<Object> vertexQueue = new LinkedList<>();
+    ArrayList<ArrayList<Object>> graph = new ArrayList<>();
+    int v;
+
+    public GraphImplementation(int numNodes)
+    {
+        v=numNodes;
+        for (int i=0; i<v; ++i)
+            graph.add(new ArrayList<Object>());
+    }
+
+    public void addEdge(Object v, Object u)
+    {
+        graph.get((Integer) v).add(u);
+        graph.get((Integer) u).add(v);
+    }
+
+    public List BFTraversal(Object originVertex, int o)
+    {
+        boolean visited[] = new boolean[v];
+
+        originVertex=true; //(somehow mark origin vertex as visited)
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        queue.add(o);
+        traversalOrder.add(originVertex);
+        //traversalOrder.enqueue(originVertex);
+
+        vertexQueue.add(originVertex);
+
+        while (queue.size() != 0)
+        {
+            // Dequeue a vertex from queue and print it
+            o = queue.poll();
+            System.out.print(o+" ");
+    }
+
+
+    /**
+         * method implementing depth first traversal of graph
+         */
+        class DFT {
+            int numvertices;
+            private LinkedList<Integer> adj_list[];
+            int node;
+
+            void addEdge(int v, int w) {
+                adj_list[v].add(w); // Add w to v's list.
+            }
+
+            void DFSUtil(int numvertices, boolean[] visited) {
+                // Mark the current node as visited and print result
+                if (visited[v] == true) {
+                    System.out.println(node);
+                }
+                System.out.print(v + " "); //spacing for formatting purposes
+                Iterator<Integer> i = adj_list[v].listIterator();
+                while (i.hasNext()) {
+                    int next = i.next();
+                    if (!visited[next])
+                        DFSUtil(next, visited); //recursive function to get vertices adjacent to the vertex
+                }
+            }
+            void DFS()
+            {
+                boolean[] visited = new boolean[v];
+                
+                for (int i = 0; i < v; ++i)
+                    if (!visited[i])
+                        DFSUtil(i, visited);
+            }
+            }
+            return graph;
+}
+}
